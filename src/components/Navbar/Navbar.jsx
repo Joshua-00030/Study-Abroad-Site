d4e0c1cc3bae03c572f7194a3a08898a459d3dd4
Joshua Romero <ggc638@my.utsa.edu>
2023-05-25 19:56:01 -0500

    moving down a folder

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+        <div className='Navbar'>
+            <p>Navbar</p>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import './Sidebar.css';
+
+
+function Sidebar() {
+    return (
+        <div className='Sidebar'>
+            <p>Sidebar</p>
+        </div>
+    )
+}
+
+export default Sidebar;
