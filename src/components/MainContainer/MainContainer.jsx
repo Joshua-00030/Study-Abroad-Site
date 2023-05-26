ac2711c866e624771b4fd3a5a873b68ebddb1726
Joshua Romero <ggc638@my.utsa.edu>
2023-05-25 20:50:05 -0500

    now trying to fix swiper import

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -2,7 +2,6 @@ import './MainContainer.css'
 import MainContent from '../MainContent/MainContent'
 import Sidebar from '../Sidebar/Sidebar'
 import Navbar from '../Navbar/Navbar'
-import "swiper/react"
 import { Swiper, SwiperSlide } from "swiper/react"
 // import required modules
 import { EffectCoverflow, Pagination } from "swiper"
